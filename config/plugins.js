a362d7bb0cc6ee44c8913b90c5ec6379c722393c
PierreRoussel <pierre.roussel01@free.fr>
2023-06-22 18:44:20 +0200

    add populate deep

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,9 @@
 module.exports = ({env}) => ({
+  'strapi-plugin-populate-deep': {
+    config: {
+      defaultDepth: 5, // Default is 5
+    }
+  },
   upload: {
     config: {
       provider: "cloudinary",
